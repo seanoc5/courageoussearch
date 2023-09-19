02526dccf5b926efe8e68182ae7e84adb06d7521
seanoc5@gmail.com <sean.oconnor@lucidworks.com>
2023-09-19 06:40:17 -0400

    adding autoComplete

--- grails-app/controllers/com/oconeco/TopicController.groovy
+++ grails-app/controllers/com/oconeco/TopicController.groovy
@@ -2,9 +2,16 @@ package com.oconeco
 
 class TopicController {
 
-    static scaffold = Topic
+    TopicService topicService
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static scaffold = Topic
 
+    def autoComplete() {
+//        List<Topic> topics = topicService.list(params)
+        List<Topic> topics = Topic.findAllByLabelIlike("%${params.term}%")
+        log.debug "TopicController autocomplete, params: $params, results: ${topics*.label}"
+        respond topics, model: [topicCount: topicService.count()]
+    }
 
 }
